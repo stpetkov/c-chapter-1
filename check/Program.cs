d8ad84860655c404c7dbdf752219aea585a9615e
Stanimir <stpetkov4@gmail.com>
2016-10-02 12:28:19 +0300

    Add project files.

---
+++ check/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Добър ден");
+        int a = 45;
+        Console.WriteLine(a+10);
+        }
+    }
+
