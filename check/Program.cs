6c062d12e742da56bb8beb93e82e3f238566a8af
Stanimir <stpetkov4@gmail.com>
2016-10-02 13:53:45 +0300

    Some of the task in c#

--- check/Program.cs
+++ check/Program.cs
@@ -8,7 +8,17 @@
         {
         Console.WriteLine("Добър ден");
         int a = 45;
-        Console.WriteLine(a+10);
+        Console.WriteLine(DateTime.Now);
+        Console.WriteLine(Math.Sqrt(12345));
+        for(int i = 2; i < 101; i++)
+        {
+            if (i % 2 != 0) { Console.WriteLine("-" + i); }
+            else { Console.WriteLine(i); }
+        }
+        int age= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(age);
+        Console.WriteLine("After 10 years you will be "+(age+10)+" years old");
+        uint g=12;
         }
     }
 
